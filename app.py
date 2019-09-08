f31ab110069d379ffcfd9f13a7c7fba5947b3775
Ubuntu <ubuntu@ip-172-31-21-190.ec2.internal>
2019-09-08 13:41:26 +0000

    test

--- app.py
+++ app.py
@@ -23,5 +23,5 @@ def add_program():
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-        port=int(os.environ.get('PORT')),
-        debug=True)
+            port=int(os.environ.get('PORT')),
+            debug=True)
