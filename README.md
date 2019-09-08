81ee3068991df811afbce6714c7851f056906387
Ubuntu <ubuntu@ip-172-31-21-190.ec2.internal>
2019-09-08 12:44:40 +0000

    Added base.html, add_program.html and added navbar.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# Calisthenics Project
+# [Calisthenics Project](https://calisthenics-project.herokuapp.com/)
 * The main purpose of this project is to motivate people to a healthier lifestyle through training.
+
+## UX
+
+## Features
+
+## Technologies used
+* [HTML](https://www.w3schools.com/html/) 
+* [Heroku](https://www.heroku.com/)
+
+## Testing
+
+## Deployment
+
+## Media
+
+## Credits
--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+
+app.config["MONGO_DBNAME"] = 'calisthenics-project'
+app.config["MONGO_URI"] = 'mongodb+srv://root:pasol005@myfirstcluster-qno2b.mongodb.net/calisthenics?retryWrites=true&w=majority'
+
+
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return "Hello World!"
+@app.route('/home')
+def home():
+    return render_template('index.html', training_programs=mongo.db.training_programs.find())
     
+@app.route('/add_program')
+def add_program():
+    return render_template('add_program.html')
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
